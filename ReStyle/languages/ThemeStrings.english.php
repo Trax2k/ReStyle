0a651c4d51cb2b77b1b8a84c8279e4c7c62ece86
Trax <jplant01@gmail.com>
2018-12-12 18:55:09 +0000

    Initial Commit

---
+++ ReStyle/languages/ThemeStrings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.1; Theme Strings
+
+$txt['facebook_url'] = 'Your Facebook URL';
+$txt['twitter_url'] = 'Your Twitter URL';
+$txt['youtube_url'] = 'Your Youtube URL';
+$txt['google_url'] = 'Your Google+ URL';
+$txt['rss_url'] = 'RSS URL';
+?>
